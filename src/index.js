de095650fb9deae665172ea069dff68d19f530d2
Gustavo <gtva5446@gmail.com>
2025-04-15 14:37:16 -0300

    sentry

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HashRouter as Router } from 'react-router-dom';
-// import * as Sentry from "@sentry/react";
+import * as Sentry from "@sentry/react";
 
-// Sentry.init({
-//   dsn: "https://2509937a93fc92ab2d96a561ea7452ff@o4508960864141312.ingest.us.sentry.io/4508960870301696",
-//   integrations: [Sentry.browserTracingIntegration()],
-//   tracePropagationTargets: ["https://myproject.org", /^\/api/],
-//   });
+Sentry.init({
+  dsn: "https://2509937a93fc92ab2d96a561ea7452ff@o4508960864141312.ingest.us.sentry.io/4508960870301696",
+  integrations: [Sentry.browserTracingIntegration()],
+  tracePropagationTargets: ["https://myproject.org", /^\/api/],
+  });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
